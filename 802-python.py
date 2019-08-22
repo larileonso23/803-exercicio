d09b676c2389fcd3c792e68a9ad3f18dc10a1435
larissa <larissaleons51@gmail.com>
2019-08-22 16:49:40 -0300

    larissa

--- 802-python.py
+++ 802-python.py
@@ -1,28 +1,28 @@
-class Animal:
-    def __init__(self, nome, dono):
-        self.nome = nome
-        self.dono = dono
+#class Animal:
+ #   def __init__(self, nome, dono):
+  #      self.nome = nome
+  #      self.dono = dono
 
-    def comer(self):
-        print('Nhom nhom')
+   # def comer(self):
+    #    print('Nhom nhom')
 
-class Gato(Animal):
+#class Gato(Animal):
 
-    def __init__(self, nome, dono, raca):
+   # def __init__(self, nome, dono, raca):
 
       
-        super().__init__(nome, dono)
-        self.raca = raca
+       # super().__init__(nome, dono)
+       # self.raca = raca
 
-    def miar(self):
-        print('Minhauuuuu')
+   # def miar(self):
+       # print('Minhauuuuu')
 
-class Cachorro(Animal):
-    def latir(self):
-        print('Au auuuu')
+#class Cachorro(Animal):
+    #def latir(self):
+       # print('Au auuuu')
 
-gato = Gato('xuxuzinho', 'Matheus', 'siames')
-cachorro = Cachorro('rex', 'Groger')
-animal = Animal('nome', 'dono')
+#gato = Gato('xuxuzinho', 'Matheus', 'siames')
+#cachorro = Cachorro('rex', 'Groger')
+#animal = Animal('nome', 'dono')
 
-print(gato.raca)
+#print(gato.raca)
